2366da6e8dd374961b55353d6ef3b577947c39e3
Garett Foster <fostgare@oregonstate.edu>
2026-08-28 16:36:56 -0500

    Fixed style issues

--- inflammation/models.py
+++ inflammation/models.py
@@ -15,20 +15,20 @@ def load_csv(filename):
 
     :param filename: Filename of CSV to load
     """
-    return np.loadtxt(fname=filename, delimiter=',')
+    return np.loadtxt(fname = filename, delimiter = ',')
 
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array."""
-    return np.mean(data, axis=0)
+    return np.mean(data, axis = 0)
 
 
 def daily_max(data):
     """Calculate the daily max of a 2D inflammation data array."""
-    return np.max(data, axis=0)
+    return np.max(data, axis = 0)
 
 
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
-    return np.min(data, axis=0)
+    return np.min(data, axis = 0)
 
